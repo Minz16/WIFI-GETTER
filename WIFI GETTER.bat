28a6eba233043dd35ffc1373108d476eee34cfbf
Minz16 <38834696+Minz16@users.noreply.github.com>
2021-05-20 09:04:56 +0200

    Update WIFI GETTER.bat

--- WIFI GETTER.bat
+++ WIFI GETTER.bat
@@ -17,4 +17,23 @@ if %input%==credits(goto c) else(goto b)
 :c
 cls
 echo Code version 1.00
-echo Copyright 2020 
+echo Copyright 2020
+echo.
+echo $: £unLˆ
+pause
+----------------------------
+:hlp
+echo Copy the name of the WIFI by selecting with the mouse and pressing right click.
+echo.
+echo To paste the copied text, press right click again.
+echo.
+echo You will find the network key somewhere at the bottom
+echo.
+set /p qst=Press Enter to get back
+goto a
+-----------------------------
+:b
+netsh wlan show profile name=%input% key=clear
+pause
+-----------------------------
+$: £unLˆ
