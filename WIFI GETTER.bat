01f6543fe525b930688e0a62cc06fe540bf50154
Minz16 <38834696+Minz16@users.noreply.github.com>
2021-08-18 14:59:33 +0200

    Add files via upload

--- WIFI GETTER.bat
+++ WIFI GETTER.bat
@@ -6,21 +6,23 @@ goto a
 :a
 cls
 netsh wlan show profile
-set /p input=Enter Name
+set /p input=Enter Name: 
 goto iffer
 ----------------------------
 :iffer
-if %input%==help(goto hlp)
-if %input%==?(goto hlp)
-if %input%==credits(goto c) else(goto b)
+if %input%==help (goto hlp)
+if %input%==? (goto hlp)
+if %input%==credits (goto c) else (goto b)
+exit
 ----------------------------
 :c
 cls
-echo Code version 1.00
-echo Copyright 2020
+echo Code version 1.1.0
+echo Copyright forever
 echo.
 echo $: £unLˆ
-pause
+set /p blank=  
+goto a
 ----------------------------
 :hlp
 echo Copy the name of the WIFI by selecting with the mouse and pressing right click.
@@ -29,11 +31,21 @@ echo To paste the copied text, press right click again.
 echo.
 echo You will find the network key somewhere at the bottom
 echo.
-set /p qst=Press Enter to get back
+set /p qst=Press Enter to get back 
 goto a
 -----------------------------
 :b
 netsh wlan show profile name=%input% key=clear
-pause
+set /p q=Finished?(Y/N)
+if %q%==Y (goto end)
+if %q%==y (goto end)
+if %q%==N (goto a)
+if %q%==n (goto a) else (exit)
+exit
+-----------------------------
+:end
+echo thx for use
+timeout 3
+exit
 -----------------------------
 $: £unLˆ
