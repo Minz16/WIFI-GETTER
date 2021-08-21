40066da923778073bc0c1d9a67a2f042eb37fc00
Minz16 <38834696+Minz16@users.noreply.github.com>
2021-08-21 20:06:56 +0200

    Add files via upload

--- WIFI-GETTER.bat
+++ WIFI-GETTER.bat
@@ -12,12 +12,12 @@ goto iffer
 :iffer
 if %input%==help (goto hlp)
 if %input%==? (goto hlp)
-if %input%==credits (goto c) else (goto b)
+if %input%==credits (goto c) else (goto d)
 exit
 ----------------------------
 :c
 cls
-echo Code version 2.1.0
+echo Code version 2.2.0
 echo Copyright forever
 echo.
 echo $: £unLˆ
@@ -31,7 +31,7 @@ echo To paste the copied text, press right click again.
 echo.
 echo You will find the network key somewhere at the bottom
 echo.
-echo If you agree, the password will be copied into a folder called WIFI in a file called p.txt.
+echo The password will be copied into a folder called WIFI in a file called p.txt.
 echo.
 set /p qst=Press Enter to get back 
 goto a
@@ -41,20 +41,17 @@ if EXIST WIFI (goto extra)
 mkdir WIFI
 :extra
 cd WIFI
-netsh wlan show profile name=%input% key=clear >> p.txt
-goto end
+goto b
 -----------------------------
 :b
+netsh wlan show profile name=%input% key=clear >> p.txt
+cls
 netsh wlan show profile name=%input% key=clear
-:f
-set /p q=Save?(Y/N) 
-if %q%==Y (goto d)
-if %q%==y (goto d)
-if %q%==N (goto end)
-if %q%==n (goto end) else (goto f)
-exit
------------------------------
-:end
+set /p q=Finished?(Y/N): 
+if %q%==Y (exit)
+if %q%==y (exit)
+if %q%==N (goto a)
+if %q%==n (goto a) else (exit)
 exit
 -----------------------------
 $: £unLˆ
